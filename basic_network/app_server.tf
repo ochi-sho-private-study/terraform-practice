f2c23876bd3fc32fe707f97d978dfe34b3e62ef5
ochi-sho-private-study <ochisyoji1118@gmail.com>
2024-01-08 03:26:17 +0900

    原因特定: セキュリティグループをインスタンスに紐づけてなかった

--- basic_network/app_server.tf
+++ basic_network/app_server.tf
@@ -1,36 +1,17 @@
-data "aws_ami" "latest_amzn2" {
-  owners      = ["amazon"]
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
 resource "aws_key_pair" "app_server" {
   key_name   = "ec2_key"
   public_key = file("~/Downloads/ec2_key.pub")
 }
 
-# インスタンス
 resource "aws_instance" "app_server" {
-  instance_type = "t3.micro" # インスタンスタイプは任意に設定する
+  ami           = "ami-0dfa284c9d7b2adad"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_1a.id
 
+  key_name               = aws_key_pair.app_server.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
-  ami = data.aws_ami.latest_amzn2.id
-
-  subnet_id = aws_subnet.public_1a.id
-
-  key_name = aws_key_pair.app_server.key_name
-
   tags = {
-    Name = "app_server" # インスタンス名
-  }
-
-  lifecycle {
-    ignore_changes = [
-      ami # インスタンスに変更を加えようとしたら、AMIが新しくなっていてインスタンス再作成が要求されるのを防止するため
-    ]
+    Name = "app_server"
   }
 }
